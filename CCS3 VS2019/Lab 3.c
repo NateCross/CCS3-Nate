5fd8771854fd56a975f0a52f5ae56e12687548b8
Nathan Angelo Cruz <jonathan.a.b.cross@gmail.com>
2021-02-11 11:35:06 +0800

    Updated Lab3 with loop

--- CCS3 VS2019/Lab 3.c
+++ CCS3 VS2019/Lab 3.c
@@ -6,19 +6,25 @@ int main() {
 
     //TODO: look for more elegant solution for negative numbers
 
-    int input[100], highestValue = -99999; //highestValue is set at that to account for negative numbers. If it would start at 0, highestValue would never change
+    int input[100], runAgain = 0, highestValue = -99999; //highestValue is set at that to account for negative numbers. If it would start at 0, highestValue would never change
 
-    //Input for each of the values. Nothing special here.
-    for (int inputCounter = 1; inputCounter <= 5; inputCounter++) {
-        printf("Input %d: ", inputCounter);
-        scanf("%d", &input[inputCounter]);
+    do {
+        //Input for each of the values. Nothing special here.
+        for (int inputCounter = 1; inputCounter <= 5; inputCounter++) {
+            printf("Input %d: ", inputCounter);
+            scanf("%d", &input[inputCounter]);
 
-        //Compares if the number input is higher than what was already input. If higher, it will replace highestValue.
-        if (input[inputCounter] > highestValue)
-            highestValue = input[inputCounter];
-    }
+            //Compares if the number input is higher than what was already input. If higher, it will replace highestValue.
+            if (input[inputCounter] > highestValue)
+                highestValue = input[inputCounter];
+        }
 
-    printf("Highest value: %d", highestValue);
+        printf("Highest value: %d \n", highestValue);
+
+        //Code to loop it again. It's in a do-while to ensure this runs at least once.
+        printf("Enter 1 to quit. Enter anything else to continue: ");
+        scanf("%d", &runAgain);
+    } while(runAgain != 1);
 
     return 0;
 }
